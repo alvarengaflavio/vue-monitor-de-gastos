973adb51f5e41a10552316578a0961b9cd5083df
alvalenda <banysan@msn.com>
2024-02-08 17:26:26 -0300

    Adiciona componente de saldo à página principal

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <Header />
+  <div class="container">
+    <Balance />
+  </div>
 </template>
 
 <script setup lang="ts">
 import Header from './components/header.vue'
+import Balance from './components/balance.vue'
 </script>

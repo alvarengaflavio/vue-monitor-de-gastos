3932fa646f4deee883d0b3b2cd0aa412adc8567a
alvalenda <banysan@msn.com>
2024-02-08 17:36:35 -0300

    Adicionado componente de adicionar transações

---
+++ src/components/add-transactions.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>Adicionar Transação</h3>
+  <form id="form">
+    <div class="form-control">
+      <label for="text">Descrição</label>
+      <input type="text" id="text" placeholder="Descrição da transação" />
+    </div>
+    <div class="form-control">
+      <label for="amount"
+        >Valor <br />
+        (negativo - despesa, positivo - renda)</label
+      >
+      <input type="number" id="amount" placeholder="Insira o valor" />
+    </div>
+    <button class="btn">Adicionar transação</button>
+  </form>
+</template>

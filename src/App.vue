8be437c798e0156ce889f7e60721783db6afdfba
alvalenda <banysan@msn.com>
2024-02-08 17:30:00 -0300

    Adicionado componente IncomeExpenses ao App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <Header />
   <div class="container">
     <Balance />
+    <IncomeExpenses />
   </div>
 </template>
 
 <script setup lang="ts">
 import Header from './components/header.vue'
 import Balance from './components/balance.vue'
+import IncomeExpenses from './components/income-expenses.vue'
 </script>

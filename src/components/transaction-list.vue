87259ddb8f2640b64dfc6b2c86fac53475d0daa6
alvalenda <banysan@msn.com>
2024-02-08 17:33:10 -0300

    Adiciona lista de transações ao componente transaction-list.vue

---
+++ src/components/transaction-list.vue
@@ -0,0 +1,11 @@
+<template>
+  <h3>Histórico de Transações</h3>
+  <ul id="list" class="list">
+    <li class="minus">
+      Dinheiro <span>-R$400</span><button class="delete-btn">x</button>
+    </li>
+    <li class="plus">
+      Pagamento <span>R$800</span><button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>

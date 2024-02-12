1f6fc537aa1abab80486eb4ca393d625bfcf6829
Flavio Alvarenga <flavio.alva@outlook.com>
2024-02-12 20:25:19 -0300

    Atualiza exibição do saldo

--- src/components/balance.vue
+++ src/components/balance.vue
@@ -1,4 +1,13 @@
 <template>
   <h4>Seu Saldo</h4>
-  <h1 id="balance">R$ 0.00</h1>
+  <h1 id="balance">R$ {{ props.total }}</h1>
 </template>
+
+<script setup lang="ts">
+const props = defineProps({
+  total: {
+    type: String,
+    required: true,
+  },
+})
+</script>

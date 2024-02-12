8679c227fc9c7a1fe625109830f88d79c70c2fa9
Flavio Alvarenga <flavio.alva@outlook.com>
2024-02-12 20:20:23 -0300

    Adiciona lista de transações com dados iniciais

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <div class="container">
     <Balance />
     <IncomeExpenses />
-    <TransactionList />
+    <TransactionList :transactions="transactions" />
     <AddTransactions />
   </div>
 </template>
@@ -14,4 +14,13 @@ import Balance from './components/balance.vue'
 import IncomeExpenses from './components/income-expenses.vue'
 import TransactionList from './components/transaction-list.vue'
 import AddTransactions from './components/add-transactions.vue'
+
+import { ref } from 'vue'
+
+const transactions = ref([
+  { id: 1, text: 'Flores', amount: -20 },
+  { id: 2, text: 'Salário', amount: 300 },
+  { id: 3, text: 'Livro', amount: -10 },
+  { id: 4, text: 'Câmera', amount: 150 },
+])
 </script>

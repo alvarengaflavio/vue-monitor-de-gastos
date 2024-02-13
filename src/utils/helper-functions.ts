2d2598de4e24c7f276c989d485dd25b2ba323877
Flavio Alvarenga <flavio.alva@outlook.com>
2024-02-13 18:07:11 -0300

    Adiciona funções de auxílio e salvamento no local de armazenamento

--- src/utils/helper-functions.ts
+++ src/utils/helper-functions.ts
@@ -1,5 +1,12 @@
+import { Ref } from 'vue'
+import { Transaction } from './types'
+
 export const uid = () =>
   String(Date.now().toString(32) + Math.random().toString(16)).replace(
     /\./g,
     ''
   )
+
+export const saveToLocalStorage = (transactions: Ref<Transaction[]>) => {
+  localStorage.setItem('transactions', JSON.stringify(transactions.value))
+}

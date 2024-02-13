efcfed6f2e5ea9c784ea53c473bec3aee42e4dbe
Flavio Alvarenga <flavio.alva@outlook.com>
2024-02-13 09:39:57 -0300

    Atualizar exibição de renda e despesas

--- src/components/income-expenses.vue
+++ src/components/income-expenses.vue
@@ -2,11 +2,24 @@
   <div class="inc-exp-container">
     <div>
       <h4>Renda</h4>
-      <p id="money-plus" class="money plus">+R$0.00</p>
+      <p id="money-plus" class="money plus">R$ {{ props.income }}</p>
     </div>
     <div>
       <h4>Despesas</h4>
-      <p id="money-minus" class="money minus">-R$0.00</p>
+      <p id="money-minus" class="money minus">R$ {{ props.expense }}</p>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const props = defineProps({
+  income: {
+    type: String,
+    required: true,
+  },
+  expense: {
+    type: String,
+    required: true,
+  },
+})
+</script>

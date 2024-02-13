4fe2038767531577ee15819719374d27c175f01c
Flavio Alvarenga <flavio.alva@outlook.com>
2024-02-13 09:53:01 -0300

    Atualiza o componente add-transactions.vue com a lógica de validação do formulário

--- src/components/add-transactions.vue
+++ src/components/add-transactions.vue
@@ -1,17 +1,42 @@
 <template>
   <h3>Adicionar Transação</h3>
-  <form id="form">
+  <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
       <label for="text">Descrição</label>
-      <input type="text" id="text" placeholder="Descrição da transação" />
+      <input
+        type="text"
+        id="text"
+        v-model="text"
+        placeholder="Descrição da transação"
+      />
     </div>
     <div class="form-control">
       <label for="amount"
         >Valor <br />
         (negativo - despesa, positivo - renda)</label
       >
-      <input type="number" id="amount" placeholder="Insira o valor" />
+      <input
+        type="number"
+        id="amount"
+        v-model="amount"
+        placeholder="Insira o valor"
+      />
     </div>
     <button class="btn">Adicionar transação</button>
   </form>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const text = ref('')
+const amount = ref('')
+
+const onSubmit = () => {
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Por favor, adicione uma descrição e valor')
+  } else {
+    console.log(text.value, amount.value)
+  }
+}
+</script>

adcef6703a181a9dbb76570e3ce86759e4a95a86
Flavio Alvarenga <flavio.alva@outlook.com>
2024-02-13 10:14:45 -0300

    Adiciona biblioteca de notificações Vue Toastification e atualizações de estilo

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+import toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 import App from './App.vue'
+import './style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(toast)
+app.mount('#app')
